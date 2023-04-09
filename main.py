4e05d083e2afadc147493f8e55aed3ef1fac06b8
KamilHann <hannanov.kamil.work@gmail.com>
2023-04-09 08:33:29 +0300

    added currency validation

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ import json
 import fastapi
 
 
+def get_currencies():
+    curs = []
+    with open("currencies") as file:
+        for line in file:
+            cur = line.strip()
+    return curs
+
+
 def convert(from_cur: str, *to_cur: tuple[str]) -> float | None:
     rates = get_rates(from_cur)
     returning_rates = {}
+    actual_curs = get_currencies()
 
     if type(rates) == dict:
         for cur in to_cur:
-            rate = rates[cur]
-
-            returning_rates[cur] = rate
+            if cur in actual_curs:
+                returning_rates[cur] = rates[cur]
 
     return returning_rates
 
 
 def get_rates(from_cur: str) -> dict | None:
+    if from_cur not in get_currencies():
+        return
+
     url = f"https://api.exchangerate-api.com/v4/latest/{from_cur}"
 
     response = requests.get(url)
